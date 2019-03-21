d5fcc7b12f927f44ca4b4c7af8044bf4f1ff7fdd
Женя Босюк <bosuk21@gmail.com>
2019-03-21 17:09:47 +0200

    Lab1 C++ 1-1 1-2

---
+++ LAB1-C++/LAB1-2-C++.cpp
@@ -0,0 +1,70 @@
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	struct peoples {
+		char sName[30];//Фамилия
+		char Name[30];//Имя
+		char thName[30];//Отчество
+		char work[30];//Занимаемая должность
+		short Salary;//Зарплата
+		short WDay, WMonth, WYear;//Дата
+	};
+
+	short AvrgSalary = 10000;
+	char s;
+	int Day, Month, Year;
+	cout << "Введите сегодняшнюю дату Д/М/Г например: 12.06.2019\n";
+	cin >> Day >> s >> Month >> s >> Year;
+	cout << "\n";
+
+	short n;
+	cout << "Введите коол людей:\n";
+	cin >> n;
+	cin.ignore();
+	peoples people[10];
+
+	for (int i = 0; i < n; i++) {
+
+		cout << "Введите Фамилию Рабочего "<< endl;
+		cin.getline(people[i].sName, 30);
+		cout << "Введите имя Рабочего\n";
+		cin.getline(people[i].Name, 30);
+		cout << "Введите отчество Рабочего\n";
+		cin >> (people[i].thName);
+		cout << "Введите должность Рабочего\n";
+		cin >> (people[i].work);
+		cout << "Введите зарплату Рабочего\n";
+		cin >> (people[i].Salary);
+		cout << "Введите год рождения Рабочего Д/М/Г например: 12.06.1996\n";
+		cin >> (people[i].WDay) >> s >> (people[i].WMonth) >> s >> (people[i].WYear);
+		cin.ignore();
+
+	}
+
+	for (int i = 0; i < n; i++) 
+	{
+		if (people[i].Salary > AvrgSalary && people[i].WYear - Year < 30 || people[i].Salary > AvrgSalary && people[i].WYear - (Year - 1) < 30 && people[i].WMonth >= Month && people[i].WDay >= Day) 
+		{
+			cout << endl;
+			cout << "Фамилия Рабочего\n";
+			cout << people[i].sName << endl;
+			cout << "Имя Рабочего\n" ;
+			cout << people[i].Name << endl;
+			cout << "Отчество Рабочего\n";
+			cout << (people[i].thName) << endl;
+			cout << "Должность Рабочего\n";
+			cout << (people[i].work) << endl;
+			cout << "Зарплата Рабочего\n";
+			cout << (people[i].Salary) << endl;
+			cout << "год рождения Рабочего Д/М/Г например: 12.06.1996\n";
+			cout << (people[i].WDay) << s << (people[i].WMonth) << s << (people[i].WYear) << endl;
+		}
+
+	}
+
+	return 0;
+}
--- LAB1-C++/LAB1-C++.cpp
+++ LAB1-C++/LAB1-C++.cpp
@@ -1,21 +1,59 @@
-﻿// LAB1-C++.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include "pch.h"
+﻿#include "pch.h"
 #include <iostream>
+using namespace std;
 
 int main()
-{
-    std::cout << "Hello World!\n"; 
+{ 
+	setlocale(LC_ALL, "Rus");
+	struct Shop {
+		char Seller[30];
+		char ProductName[50];
+		short Price;
+		short PDay, PMonth, PYear;
+	};
+	
+	char s;
+	int Day, Month, Year;
+	cout << "Введите сегодняшнюю дату Д/М/Г например: 12.06.2019\n";
+	cin >> Day >> s >> Month >> s >> Year;
+	cout << "\n";
+
+	short n;
+	cout << "Введите коол товаров:\n";
+	cin >> n;
+	Shop product[10];
+
+	cout << "Введите харрактеристики товаров.\n"<<endl;
+	cin.ignore();
+
+	for (int i = 0; i < n; i++) {
+
+		cout << "Введите имя продавца №" << i <<endl;
+		cin.getline(product[i].Seller, 30);
+		cout << "Введите имя товара\n";
+		cin.getline(product[i].ProductName, 50);
+		cout << "Введите цену продажи\n";
+		cin >> (product[i].Price);
+		cout << "Введите год продажи Д/М/Г например: 12.06.2019\n";
+		cin >> (product[i].PDay) >> s >> (product[i].PMonth) >> s >> (product[i].PYear);
+		cin.ignore();
+
+	}
+
+	cout << "Проданы Менее года назад\n" << endl;
+	for (int i = 0; i < n; i++) 
+	{
+		if (product[i].PYear == Year || product[i].PYear == Year - 1 && product[i].PMonth == Month && product[i].PDay >= Day || product[i].PYear == Year-1 && product[i].PMonth > Month ) {
+			cout << endl;
+			cout <<"Продавец: "<< product[i].Seller << endl;
+			cout <<"Имя товара: "<< product[i].ProductName << endl;
+			cout <<"Цена: "<< product[i].Price << endl ;
+			cout <<"Дата продажи: " << (product[i].PDay) << s << (product[i].PMonth) << s << (product[i].PYear);
+			
+
+		}
+				
+	}
+	
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
